1a0bfb11742610b6349f239d44f1cd013833cab0
susiesko <susie.sko@gmail.com>
2021-03-11 21:33:06 -0800

    created base start logging frame;
    added axios file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { Route, Switch } from 'react-router';
 import Layout from './containers/Layout/Layout';
 import Profile from './containers/Profile/Profile';
+import DogLog from './containers/DogLog/DogLog';
 
 function App() {
   return (
     <div className="App">
       <Layout>        
         <Switch>
-          <Route exact path="/"><Profile/></Route>
+          <Route exact path="/"><DogLog/></Route>
+          <Route exact path="/doglog"><DogLog/></Route>
           <Route exact path="/profile"><Profile/></Route>
           <Route exact path="/" render={() => <div>hello world</div>}/>
         </Switch>
